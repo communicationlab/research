bac527be25324dc331176dad97af257d14efe437
Amirul Islam <72323862+amirul2k9@users.noreply.github.com>
2026-07-28 00:08:48 +0600

    Update data.js

--- js/data.js
+++ js/data.js
@@ -112,6 +112,10 @@ const PEOPLE = [
       "He was a Postdoctoral Researcher at the Visual Artificial Intelligence Laboratory, Oxford Brookes University, UK, where he led the MAESTRO Jr. project on multimodal cognitive-workload assessment in surgical environments.",
       "He earned his Ph.D. in Computing and Electronic Systems from the University of Essex, UK (2022), an M.Sc. in Electronics Engineering from Kookmin University, South Korea (2017), and a B.Sc. in EEE from KUET, Bangladesh (2014). He remains active in interdisciplinary research and welcomes collaboration."
     ],
+     professional: [
+      { deg: "Assistant Professor", org: "AIUB, Bangladesh", year: "2025" },
+      { deg: "Postdoctoral Researcher", org: "Oxford Brookes University, UK", year: "2023" }
+    ],
     education: [
       { deg: "Ph.D. in Computing & Electronic Systems", org: "University of Essex, UK", year: "2022" },
       { deg: "M.Sc. in Electronics Engineering", org: "Kookmin University, South Korea", year: "2017" },
